248ea8b89713db57208541044d7f3a7d6a0ae935
Kelvinator07 <kelvinator4leo@gmail.com>
2020-03-05 23:19:32 +0100

    Backend Resources

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (id, username, password, firstName, lastName, email, createdAt, roles, taskList )
+VALUES (101, 'myadmin', '$2a$10$G.Yhe0dKWQcT1JDVEDktS.pfN9PQfrpBn2l68cSUltxRjVOD2C1/S', 'My', 'Admin', 'myadmin@gmail.com', CURRENT_DATE(), 'ROLE_ADMIN', null )
